14d297f246154b3813daf2d32f78cdae3479da98
ThomasP <thomas.pareschi@campus-centre.fr>
2023-10-24 12:10:36 +0200

    V1 PAS TOUCHE

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter";
+import AppNavigator from "./component/AppNavigator";
 
-export default function () {
+export default function App() {
+  /*Laisse en place le splash creen pendant qu'on récupère les ressources nécessaires */
+  SplashScreen.preventAutoHideAsync();
+
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        //Chargement des fonts
+        await Font.loadAsync({ Inter_400Regular, Inter_600SemiBold });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>CouCou</Text>
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <AppNavigator />
     </View>
   );
 }
---
+++ screens/Produits.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default function () {
+  return (
+    <View>
+      <Text>hey</Text>
+    </View>
+  );
+}
