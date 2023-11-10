963cad80e67acc2eab8217d8dd47fb70fd4b988d
ThomasP <thomas.pareschi@campus-centre.fr>
2023-11-10 16:44:39 +0100

    ajout au panier finito

--- V4/screens/Panier.js
+++ V4/screens/Panier.js
@@ -4,26 +4,19 @@ import {
   Text,
   View,
   Image,
-  Pressable,
   ActivityIndicator,
   TouchableOpacity,
-  TextInput,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GlobalStyles, StyleFiche } from "../styles/AppStyles";
-import { useNavigation } from "@react-navigation/native";
 import * as SecureStore from "expo-secure-store";
-import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function Produits({ route }) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const navigation = useNavigation();
-  const produitType = route.params?.produitType;
-  const [quantity, setQuantity] = useState("1");
 
-  const addToCart = () => {
-    console.log(`Ajouté au panier : ${quantity} x ${designation}`);
+  const addToCart = (item) => {
+    //console.log(`Ajouté au panier : ${item.quantite} x ${item.designation}`);
   };
 
   useEffect(() => {
@@ -31,9 +24,9 @@ export default function Produits({ route }) {
     const fetchDataApi = async () => {
       try {
         const Token = await SecureStore.getItemAsync("token");
-        if (!Token) {
-          navigation.navigate("Login");
-        }
+        // if (!Token) {
+        //   navigation.navigate("Login");
+        //  }(
         console.log("Début de la récupération des données depuis l'API");
 
         const newData = await fetch(
@@ -44,7 +37,7 @@ export default function Produits({ route }) {
         );
         console.log("Données récupérées avec succès depuis l'API");
         const jsonData = await newData.json();
-        const storageKey = "data_product_" + produitType;
+        const storageKey = "data_product_panier";
 
         await AsyncStorage.setItem(storageKey, JSON.stringify(jsonData));
         console.log("Données stockées avec succès dans AsyncStorage");
@@ -56,62 +49,35 @@ export default function Produits({ route }) {
         setIsLoading(false); // Arrêter l'indicateur de chargement en cas d'erreur
       }
     };
-
-    const fetchDataLocal = async () => {
-      try {
-        console.log("Début de la récupération des données locales");
-        const storageKey = "data_product_" + produitType;
-        const storedData = await AsyncStorage.getItem(storageKey);
-
-        if (storedData !== null) {
-          const parsedData = JSON.parse(storedData);
-          setData(parsedData);
-        }
-
-        setIsLoading(false); // Les données ont été chargées depuis le stockage local
-      } catch (error) {
-        console.error(
-          "Erreur lors de la récupération des données locales",
-          error,
-        );
-        setIsLoading(false); // Arrêter l'indicateur de chargement en cas d'erreur
-      }
-    };
-
-    fetchDataLocal(); // Vérifie si des données sont déjà stockées localement
     fetchDataApi(); // Charge les données depuis l'API
-  }, [produitType]);
+  }, []);
 
   function renderProfiles({ item }) {
     console.log(item);
     return (
-      <Pressable
-        onPress={() => navigation.navigate("FicheProduit", { item: item })}
-      >
-        <View style={GlobalStyles.item}>
-          <Text style={GlobalStyles.title}>{item.designation}</Text>
-          {item.image && (
-            <Image
-              source={{ uri: item.image }}
-              style={{ width: 300, height: 200 }}
-            />
-          )}
-          <TouchableOpacity
-            style={StyleFiche.addToCartButton}
-            onPress={addToCart}
-          >
-            <Text style={StyleFiche.buttonText}>-</Text>
-          </TouchableOpacity>
-          <TextInput></TextInput>
-          <TouchableOpacity
-            style={StyleFiche.addToCartButton}
-            onPress={addToCart}
-          >
-            <Text style={StyleFiche.buttonText}>-</Text>
-          </TouchableOpacity>
-          <Text style={GlobalStyles.text}>{item.total_prix}€</Text>
-        </View>
-      </Pressable>
+      <View style={GlobalStyles.item}>
+        <Text style={GlobalStyles.title}>{item.designation}</Text>
+        {item.image && (
+          <Image
+            source={{ uri: item.image }}
+            style={{ width: 300, height: 200 }}
+          />
+        )}
+        <TouchableOpacity
+          style={StyleFiche.addToCartButton}
+          onPress={addToCart}
+        >
+          <Text style={StyleFiche.buttonText}>+</Text>
+        </TouchableOpacity>
+        <Text>{item.quantite}</Text>
+        <TouchableOpacity
+          style={StyleFiche.addToCartButton}
+          onPress={addToCart({ item })}
+        >
+          <Text style={StyleFiche.buttonText}>-</Text>
+        </TouchableOpacity>
+        <Text style={GlobalStyles.text}>{item.total_prix}€</Text>
+      </View>
     );
   }
 
@@ -124,7 +90,6 @@ export default function Produits({ route }) {
           data={data}
           renderItem={renderProfiles}
           keyExtractor={(item) => item.reference}
-          style={{ marginTop: 20 }}
         />
       ) : (
         <Text>Aucune donnée disponible.</Text>
