1eb0c368f1c33f3518035761105dffbf02b0bb47
yuki_640 <640poto@gmail.com>
2023-11-16 11:12:31 +0100

    new home css

--- V4-T/screens/Home.js
+++ V4-T/screens/Home.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, Dimensions } from "react-native";
-const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+import { View, Text, Image, TouchableOpacity, FlatList, Dimensions } from "react-native";
+import { getHomeStyles } from '../styles/AppStyles';
 
 const promotionsData = [
   { id: 1, image: require("../images/choco1.png") },
   { id: 2, image: require("../images/choco2.jpg") },
   { id: 3, image: require("../images/choco3.jpg") },
   { id: 4, image: require("../images/choco4.jpg") },
-  // Ajoutez autant d'images de promotion que nécessaire avec des identifiants uniques
 ];
 
-export default function Home({ route }) {
+export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const HomeStyles = getHomeStyles();
+  const { width: screenWidth } = Dimensions.get("window");
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,7 +23,7 @@ export default function Home({ route }) {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View style={HomeStyles.container}>
       <FlatList
         data={promotionsData}
         keyExtractor={(item) => item.id.toString()}
@@ -34,68 +35,17 @@ export default function Home({ route }) {
           setCurrentIndex(newIndex);
         }}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.promotionItem}>
-            <Image source={item.image} style={styles.promotionImage} resizeMode="cover" />
+          <TouchableOpacity style={HomeStyles.promotionItem}>
+            <Image source={item.image} style={HomeStyles.promotionImage} resizeMode="cover" />
           </TouchableOpacity>
         )}
       />
-      <View style={styles.body}>
-        <Text style={styles.welcomeText}>Bienvenue sur notre application de e-commerce !</Text>
-        <TouchableOpacity style={styles.shopNowButton}>
-          <Text style={styles.shopNowButtonText}>Commencez vos achats</Text>
+      <View style={HomeStyles.body}>
+        <Text style={HomeStyles.welcomeText}>Bienvenue sur notre application de e-commerce !</Text>
+        <TouchableOpacity style={HomeStyles.shopNowButton}>
+          <Text style={HomeStyles.shopNowButtonText}>Commencez vos achats</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
-
-
-  
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 10,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    marginBottom: 10,
-  },
-  promotionItem: {
-    width: screenWidth,
-    height: screenHeight * 0.3,
-    marginBottom: -250,
-  },
-  promotionImage: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-    borderRadius: 20,
-  },
-  body: {
-    flex: 1,
-    justifyContent: "top",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  welcomeText: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  shopNowButton: {
-    backgroundColor: "#3498db",
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 50,
-  },
-  shopNowButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-});
