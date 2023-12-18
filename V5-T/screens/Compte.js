48e8823ae6cffa16ee63644df39aeef802105b72
Thomas <thomas.pareschi@hotmail.com>
2023-12-18 11:27:10 +0100

    Modification des informations dans le profil

--- V5-T/screens/Compte.js
+++ V5-T/screens/Compte.js
@@ -1,45 +1,50 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: "#000",
   },
   header: {
-    backgroundColor: '#333',
+    backgroundColor: "#333",
     paddingVertical: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   headerText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
   },
   menuContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap', // Allow items to wrap to the next line
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    flexWrap: "wrap", // Allow items to wrap to the next line
+    justifyContent: "space-around",
     paddingVertical: 10,
-    backgroundColor: '#333',
+    backgroundColor: "#333",
   },
   menuItem: {
-    backgroundColor: '#555',
+    backgroundColor: "#555",
     padding: 10,
     borderRadius: 10, // This will give you rounded corners
     margin: 5, // Add margin to create space between items
-    width: '45%', // Set width to 45% of the screen width
+    width: "45%", // Set width to 45% of the screen width
   },
   menuText: {
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
   },
-  // ... Rest of your styles remain unchanged
 });
 
-function CompteScreen () {
+function CompteScreen() {
   const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
@@ -55,22 +60,19 @@ function CompteScreen () {
         <TouchableOpacity style={styles.menuItem}>
           <Text style={styles.menuText}>Acheter à nouveau</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Profil')}>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("Profil")}
+        >
           <Text style={styles.menuText}>Votre compte</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem}>
           <Text style={styles.menuText}>Vos listes</Text>
         </TouchableOpacity>
-        
-
-
       </View>
       {/* ... Rest of your component remains unchanged */}
     </SafeAreaView>
   );
-};
-
-
+}
 
 export default CompteScreen;
-
