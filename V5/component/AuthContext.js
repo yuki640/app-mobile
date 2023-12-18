ad318320556faf35624a2e2d9f7366ba7d137273
yuki_640 <640poto@gmail.com>
2023-12-18 16:55:15 +0100

    push

--- V5/component/AuthContext.js
+++ V5/component/AuthContext.js
@@ -1,1 +1,17 @@
-export default class AuthProvider {}
+// AuthContext.js
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const signIn = () => setIsLoggedIn(true);
+    const signOut = () => setIsLoggedIn(false);
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
